4e1c8ffb0cd74ed70a06d7ade009ae58f0252314
Dmitry Gubich <gubich97@gmail.com>
2023-11-13 20:12:23 +0100

    add image route

--- app/main.py
+++ app/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.bookings.router import router as booking_router
 from app.hotels.router import router as hotel_router
+from app.images.router import router as image_router
 from app.rooms.router import router as room_router
 from app.templates.router import router as template_router
 from app.users.router import auth_router as auth_router
 
 app = FastAPI(title="Hotel Management System")
 
+app.mount("/static", StaticFiles(directory="static"), "static")
+
 app.include_router(auth_router)
 app.include_router(booking_router)
 app.include_router(room_router)
 app.include_router(hotel_router)
 app.include_router(template_router)
+app.include_router(image_router)
--- app/templates/router.py
+++ app/templates/router.py
@@ -12,15 +12,10 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/hotels", response_class=HTMLResponse)
 async def get_hotels_page(request: Request, hotels=Depends(get_hotels)):
-    date_from = datetime.today().date()
-    date_to = (datetime.today() + timedelta(days=180)).date()
     return templates.TemplateResponse(
         "hotels.html",
         {
             "request": request,
             "hotels": hotels,
-            "location": "",
-            "date_to": date_to,
-            "date_from": date_from,
         },
     )
